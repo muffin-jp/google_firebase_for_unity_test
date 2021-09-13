9c99963809e4543b68a14f93ded9bbff3ba2aaff
Utoyo <utoyovici@gmail.com>
2021-09-13 15:07:53 +0900

    Add method GenerateSHA256NonceFromRawNonce

--- Assets/InGameMoney/Scripts/AuthUtility.cs
+++ Assets/InGameMoney/Scripts/AuthUtility.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace InGameMoney
 {
@@ -19,6 +22,62 @@ namespace InGameMoney
                 return fbEx.ErrorCode == firebaseExceptionCode;
             }
             return false;
+        }
+        
+        public static string GenerateRandomString(int length)
+        {
+            if (length <= 0)
+            {
+                throw new Exception("Expected nonce to have positive length");
+            }
+
+            const string charset = "0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._";
+            var cryptographicallySecureRandomNumberGenerator = new RNGCryptoServiceProvider();
+            var result = string.Empty;
+            var remainingLength = length;
+
+            var randomNumberHolder = new byte[1];
+            while (remainingLength > 0)
+            {
+                var randomNumbers = new List<int>(16);
+                for (var randomNumberCount = 0; randomNumberCount < 16; randomNumberCount++)
+                {
+                    cryptographicallySecureRandomNumberGenerator.GetBytes(randomNumberHolder);
+                    randomNumbers.Add(randomNumberHolder[0]);
+                }
+
+                for (var randomNumberIndex = 0; randomNumberIndex < randomNumbers.Count; randomNumberIndex++)
+                {
+                    if (remainingLength == 0)
+                    {
+                        break;
+                    }
+
+                    var randomNumber = randomNumbers[randomNumberIndex];
+                    if (randomNumber < charset.Length)
+                    {
+                        result += charset[randomNumber];
+                        remainingLength--;
+                    }
+                }
+            }
+
+            return result;
+        }
+        
+        public static string GenerateSHA256NonceFromRawNonce(string rawNonce)
+        {
+            var sha = new SHA256Managed();
+            var utf8RawNonce = Encoding.UTF8.GetBytes(rawNonce);
+            var hash = sha.ComputeHash(utf8RawNonce);
+
+            var result = string.Empty;
+            for (var i = 0; i < hash.Length; i++)
+            {
+                result += hash[i].ToString("x2");
+            }
+
+            return result;
         }
     }
 }
