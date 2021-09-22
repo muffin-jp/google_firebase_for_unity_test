5fae5bde194eae8203cf11d25378d6d9ccceb233
Utoyo <utoyovici@gmail.com>
2021-09-22 11:19:43 +0900

    Fix when user login as guest and restart the app, Firebase user is reseted (it won't do auto login)

--- Assets/InGameMoney/Scripts/Guest.cs
+++ Assets/InGameMoney/Scripts/Guest.cs
@@ -15,7 +15,7 @@ namespace InGameMoney
         
         public void Validate()
         {
-            if (string.IsNullOrEmpty(userData.data.mailAddress) || string.IsNullOrEmpty(userData.data.password))
+            if (!AccountTest.Instance.SignedIn && (string.IsNullOrEmpty(userData.data.mailAddress) || string.IsNullOrEmpty(userData.data.password)))
             {
                 AccountTest.Instance.SignOutBecauseLocalDataIsEmpty();
                 return;
