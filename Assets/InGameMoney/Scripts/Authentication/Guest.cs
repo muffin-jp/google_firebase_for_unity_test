fb75a6ebcf3ad2f40f84fc8baa05a751aa106fb0
Utoyo <utoyovici@gmail.com>
2021-10-01 18:03:41 +0900

    Create Guest Auth Sign Up test script

--- Assets/InGameMoney/Scripts/Authentication/Guest.cs
+++ Assets/InGameMoney/Scripts/Authentication/Guest.cs
@@ -1,5 +1,6 @@
+using System.Threading.Tasks;
 using Firebase.Auth;
-using UnityEngine;
+using Firebase.Extensions;
 
 namespace InGameMoney
 {
@@ -7,10 +8,10 @@ namespace InGameMoney
     {
         private readonly FirebaseAuth auth;
         private readonly UserData userData;
-        
-        public Guest()
+
+        public Guest(FirebaseAuth auth)
         {
-            auth = FirebaseAuth.DefaultInstance;
+            this.auth = auth;
             userData = ((UserDataAccess)AccountManager.UserDataAccess).UserData;
         }
         
@@ -29,6 +30,80 @@ namespace InGameMoney
             }
             AccountManager.Instance.Login();
             AccountManager.Instance.UpdatePurchaseAndShop();
+        }
+
+        public async Task PerformGuestLogin()
+        {
+            AccountManager.InitPersonalData();
+            AccountManager.ResetPersonalData();
+            await SignUpUsingAnonymous();
+            ObjectManager.Instance.RegisterGuestAccount.SetActive(true);
+        }
+        
+        private async Task SignUpUsingAnonymous()
+        {
+            Print.GreenLog("Creating Gust User Account....");
+
+            var newUser = await SignInAnonymously();
+            Print.GreenLog($">>>> Firebase guest user created successfully Email {newUser.Result.Email} id {newUser.Result.UserId} DisplayName {newUser.Result.DisplayName}");
+            ProceedLogin();
+            var userDataFromInputField = AccountManager.Instance.GetDefaultUserDataFromInputField();
+            await AccountManager.Instance.SignUpToFirestoreAsync(userDataFromInputField);
+            AccountManager.Instance.WriteUserData();
+            AccountManager.Instance.Login();
+        }
+
+        public async Task<Task<FirebaseUser>> SignInAnonymously()
+        {
+            var task = auth.SignInAnonymouslyAsync().ContinueWith(signInTask => signInTask);
+
+            await task;
+            
+            if (task.Result.IsCanceled)
+            {
+                Print.GreenLog(">>>> SignInAnonymouslyAsync was canceled.");
+            }
+            
+            if (task.Result.IsFaulted)
+            {
+                Print.GreenLog($">>>>  SignInAnonymouslyAsync encountered an error: {task.Exception}");
+            }
+
+            return task.Result.IsCompleted ? task.Result : default;
+        }
+        
+        private void ProceedLogin()
+        {
+            if (ObjectManager.Instance.FirstBoot) ObjectManager.Instance.FirstBoot.SetActive(false);
+            ObjectManager.Instance.InGameMoney.SetActive(true);
+            AccountManager.Instance.SetupUI($"匿名@{auth.CurrentUser.UserId}", $"vw-guest-pass@{auth.CurrentUser.UserId}", false);
+            if (AccountManager.Instance.SignedIn)
+            {
+                Print.GreenLog($">>>> OpenGameView from OnGuestLoginButton {auth.CurrentUser.Email}");
+                AccountManager.Instance.OpenGameView();
+            }
+        }
+
+        public async void DeleteUserAsync()
+        {
+            if (auth.CurrentUser != null)
+            {
+                var deleteUser = auth.CurrentUser.DeleteAsync().ContinueWithOnMainThread(task => task);
+
+                await deleteUser;
+                if (deleteUser.Result.IsCanceled)
+                {
+                    Print.RedLog(">>>> DeleteAsync was canceled.");
+                    return;
+                }
+
+                if (deleteUser.Result.IsFaulted)
+                {
+                    Print.RedLog(">>>> DeleteAsync encountered an error: " + deleteUser.Exception);
+                    return;
+                }
+                Print.GreenLog($">>>> User deleted successfully");
+            }
         }
     }
 }
