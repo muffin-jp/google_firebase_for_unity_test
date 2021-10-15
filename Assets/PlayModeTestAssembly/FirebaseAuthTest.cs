ab7d14e4f1dee463cc8b4f0b917d951425f2d386
Utoyo <utoyovici@gmail.com>
2021-10-15 13:13:58 +0900

    SendEmailの自動テスト

--- Assets/PlayModeTestAssembly/FirebaseAuthTest.cs
+++ Assets/PlayModeTestAssembly/FirebaseAuthTest.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Net.Mail;
 using System.Threading.Tasks;
 using Firebase.Auth;
 using InGameMoney;
@@ -7,6 +9,7 @@ using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
+using AsyncOperation = UnityEngine.AsyncOperation;
 
 namespace PlayModeTestAssembly
 {
@@ -17,7 +20,16 @@ namespace PlayModeTestAssembly
         private static IEnumerable<string> LevelTestCases => new List<string> {"FirebaseAuthentication"};
         private const string Email = "test-runner@email.com";
         private const string Password = "1234567";
-        
+        private const string UserName = "Guest1";
+        private const string MailHost = "smtp.gmail.com";
+        private const int MailPort = 587;
+        private const string MailPassword = "P@ssw0rd771";
+        private const string PublisherMailAddress = "iwatanic@gmail.com";
+        private const string Messages = "Hello From Unity Test Runner";
+        private static bool sendingEmailComplete;
+        private static bool guestSignUpComplete;
+        private static bool testSendingEmailProcessComplete;
+
         [SetUp]
         public void Setup()
         {
@@ -36,9 +48,11 @@ namespace PlayModeTestAssembly
             Assert.IsTrue(loadSceneOperation.isDone, "Scene not loaded");
 
             GuestSignUp();
-
             // Wait to avoid errors missing firebase logger game object
-            yield return new WaitForSeconds(2);
+            while (!guestSignUpComplete)
+            {
+                yield return null;
+            }
         }
 
         private static async void GuestSignUp()
@@ -50,6 +64,7 @@ namespace PlayModeTestAssembly
             await guest.DeleteUserAsync();
             AccountManager.Instance.SignOut();
             Assert.IsFalse(AccountManager.Instance.SignedIn, "Should be signed out");
+            guestSignUpComplete = true;
         }
         
         private IEnumerator LoadScene(string sceneName)
@@ -165,6 +180,81 @@ namespace PlayModeTestAssembly
             await emailAuth.DeleteUserAsync();
             AccountManager.Instance.SignOut();
             Assert.IsFalse(AccountManager.Instance.SignedIn, "Should be signed out");
+        }
+
+        [UnityTest]
+        [Timeout(3600000)]
+        public IEnumerator Test5SendEmail([ValueSource(nameof(LevelTestCases))] string sceneName)
+        {
+            yield return LoadScene(sceneName);
+            Assert.IsTrue(loadSceneOperation.isDone, "Scene not loaded");
+
+            //yield return TestSendEmail();
+            GuestSignUpForEmail();
+            // Wait to avoid errors missing firebase logger game object
+            while (!testSendingEmailProcessComplete)
+            {
+                yield return null;
+            }
+        }
+
+        private static void TestSendEmail(string resultUserId)
+        {
+            var smtpClient = new SmtpClient(MailHost);
+            smtpClient.SendCompleted += SendCompleteHandler;
+            var body = GetBodyMessages(resultUserId);
+            var sendEmail = new SendEmail.Scripts.SendEmail(Email, PublisherMailAddress, "MySubject", body, smtpClient);
+            sendEmail.Send(MailPort, MailPassword);
+        }
+        
+        private static async void GuestSignUpForEmail()
+        {
+            var guest = new Guest(FirebaseAuth.DefaultInstance);
+            var newUser = await guest.SignInAnonymously();
+
+            Assert.That(newUser.Result.IsAnonymous);
+            
+            TestSendEmail(newUser.Result.UserId);
+            while (!sendingEmailComplete)
+            {
+                await Task.Yield();
+            }
+
+            await guest.DeleteUserAsync();
+            AccountManager.Instance.SignOut();
+            Assert.IsFalse(AccountManager.Instance.SignedIn, "Should be signed out");
+            testSendingEmailProcessComplete = true;
+        }
+
+        private static void SendCompleteHandler(object sender, AsyncCompletedEventArgs e)
+        {
+            var msg = (MailMessage)e.UserState;
+
+            if (e.Cancelled)
+            {
+                Debug.Log(">>>> <color=yellow>Cancelled sending message.</color>");
+            }
+            else if (e.Error != null)
+            {
+                Debug.Log($">>>> <color=red>An error has occurred.</color> Error: {e.Error}");
+            }
+            else
+            {
+                const string logText = ">>>> <color=lime>Mail has been sent.</color>";
+                Debug.Log(logText);
+            }
+            
+            Assert.IsTrue(e.Error == null, "Send Email Has Error");
+            sendingEmailComplete = true;
+            msg.Dispose();
+        }
+        
+        private static string GetBodyMessages(string resultUserId)
+        {
+            return $"User Name: {UserName}\n" +
+                   $"User Contact EmailAddress: {Email}\n" +
+                   $"User Id {resultUserId}\n" +
+                   $"Messages: \n{Messages}\n";
         }
     }
 }
